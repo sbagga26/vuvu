548f930af5f5ca9ec6a55bf2b9a5ae30acdab604
Smriti Bagga <49098552+sbagga26@users.noreply.github.com>
2019-06-10 12:03:20 +0530

    Add files via upload

---
+++ loggingHandler.py
@@ -0,0 +1,23 @@
+import logging,os ,sys, collections
+
+class loggingHandler(logging.FileHandler):
+    def __init__(self, log_queue):
+        logging.Handler.__init__(self)
+        self.log_queue = log_queue
+
+    def emit(self, record):
+        self.log_queue.append(self.format(record))
+
+
+class TailLogger(object):
+
+    def __init__(self, maxlen):
+        self._log_queue = collections.deque(maxlen=maxlen)
+        self._log_handler = loggingHandler(self._log_queue)
+
+    def contents(self):
+        return '\n'.join(self._log_queue)
+
+    @property
+    def log_handler(self):
+        return self._log_handler
